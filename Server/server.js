73539259aabc9639d45e89f916ea9968baa6d3cf
JOhn_Mustard <166864125+aditya-wuw@users.noreply.github.com>
2026-06-15 01:03:36 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: "*", 
+  origin: process.env.ALLOWED_ORIGIN, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
